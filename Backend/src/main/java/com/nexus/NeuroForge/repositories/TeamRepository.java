240fd0871ca9851da0be24c243e30a14cbe95556
JashanGill04 <jpsgill6@gmail.com>
2026-08-23 20:45:17 +0530

    Feature/ai (#49)
    * Some Fixes
    
    * repo fix

--- Backend/src/main/java/com/nexus/NeuroForge/repositories/TeamRepository.java
+++
@@ -1,8 +0,0 @@
-package com.nexus.NeuroForge.repositories;
-
-import com.nexus.NeuroForge.models.Team;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeamRepository extends JpaRepository<Team,Long> {
-    boolean existsByName(String name);
-}

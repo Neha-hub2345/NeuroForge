30273cf575563da8d935e68ec0b27dbac5ad49a8
Jashanpreet Singh <jpsgill6@gmail.com>
2026-08-23 20:44:00 +0530

    repo fix

--- Backend/src/main/java/com/nexus/NeuroForge/dto/TeamRequest.java
+++
@@ -1,8 +0,0 @@
-package com.nexus.NeuroForge.dto;
-
-public class TeamRequest {
-    private String name;
-
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-}
